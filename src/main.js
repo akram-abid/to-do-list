de10776a38b4c7069f26be4fad58115cd892c3d1
akram-abid <a.abid44778@gmail.com>
2025-01-26 08:24:02 +0100

    feat: remove unnecessary lines

--- src/main.js
+++ src/main.js
@@ -32,19 +32,18 @@ export const graphicHandler = (function () {
     const reloadTasks = () => {
         const taskListPaper = document.querySelector(".task-list-paper");
         taskListPaper.innerHTML = "";
+        let first = 0;
         const size = tasksHandler.getTasks().length;
-        console.log("this is the size of it ", size);
-        console.log("this is it ", tasksHandler.getTasks());
         for (let i = 0; i < size ; i++) {
-          console.log("this is the condition ", tasksHandler.getTasks()[i].list == graphicHandler.getCurrentProject());
-          console.log("elements of cond ", graphicHandler.getCurrentProject());
-          console.log("another element is from the list ", tasksHandler.getTasks()[i].list);
             if (
                 tasksHandler.getTasks()[i].list ==
                 graphicHandler.getCurrentProject()
             ) {
-                console.log("tryin to create an element", currentTodoProject);
                 domBuilder.taskListCreator(i);
+                if(first == 0){
+                reloadTaskDeatails(i);
+                first ++;
+                }
             }
         }
     };
@@ -61,7 +60,6 @@ export const graphicHandler = (function () {
 
         const dueDate = document.querySelector(".due-date");
         dueDate.innerHTML = tasksHandler.getTasks()[index].dueDate;
-        console.log("look its fucking working");
     };
 
     return {
@@ -96,27 +94,6 @@ function Task(title, discribtion, dueDate, priority, list, state) {
         state,
     };
 }
-
-const t1 = Task("dir jedha", "nike", "today", "high", "hak", true);
-const t2 = Task("hak berk", "nike", "today", "high", "hak", true);
-const fuck = [];
-//fuck.push(t2);
-
-//  localStorage.setItem("task", JSON.stringify(fuck));
-
-console.log("giving the order to start creating");
-graphicHandler.reloadTasks();
-console.log("done creating");
-
-const retrivedData = localStorage.getItem("tasko")
-    ? JSON.parse(localStorage.getItem("tasko"))
-    : "empty";
-console.log("hamlik tasks ", JSON.parse(localStorage.getItem("task")));
-console.log("aewedi hak ", retrivedData);
-console.log(tasksHandler.getTasks());
-
-const tmpTasks = Object.values(localStorage.getItem("task"));
-console.log("chouf hna " + Array.isArray(tmpTasks));
 
 graphicHandler.reloadTasks();
 graphicHandler.todoProjectsLoader();

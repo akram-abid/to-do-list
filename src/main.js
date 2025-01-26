253e58c55a8bd23dadfdda81f45855f30efa4b6b
akram-abid <a.abid44778@gmail.com>
2025-01-26 08:00:29 +0100

    feat complete the dynamic tasks reload using localStorage

--- src/main.js
+++ src/main.js
@@ -32,11 +32,18 @@ export const graphicHandler = (function () {
     const reloadTasks = () => {
         const taskListPaper = document.querySelector(".task-list-paper");
         taskListPaper.innerHTML = "";
-        for (let i = 0; i < tasksHandler.getTasks().length; i++) {
+        const size = tasksHandler.getTasks().length;
+        console.log("this is the size of it ", size);
+        console.log("this is it ", tasksHandler.getTasks());
+        for (let i = 0; i < size ; i++) {
+          console.log("this is the condition ", tasksHandler.getTasks()[i].list == graphicHandler.getCurrentProject());
+          console.log("elements of cond ", graphicHandler.getCurrentProject());
+          console.log("another element is from the list ", tasksHandler.getTasks()[i].list);
             if (
                 tasksHandler.getTasks()[i].list ==
                 graphicHandler.getCurrentProject()
             ) {
+                console.log("tryin to create an element", currentTodoProject);
                 domBuilder.taskListCreator(i);
             }
         }
@@ -75,32 +82,37 @@ if (JSON.parse(localStorage.getItem("todo")) == []) {
     graphicHandler.todoProjectsLoader();
     console.log(JSON.parse(localStorage.getItem("todo")));
     todoFlow.refresh();
+    console.log("giving the order to start creating");
+    graphicHandler.reloadTasks();
 }
-
-
 
 function Task(title, discribtion, dueDate, priority, list, state) {
-  return {
-      title,
-      discribtion,
-      dueDate,
-      priority,
-      list,
-      state,
-  };
+    return {
+        title,
+        discribtion,
+        dueDate,
+        priority,
+        list,
+        state,
+    };
 }
 
-const t1 = Task("dir jedha", "nike", "today", "high", "Work", true);
-const fuck = [t1];
+const t1 = Task("dir jedha", "nike", "today", "high", "hak", true);
+const t2 = Task("hak berk", "nike", "today", "high", "hak", true);
+const fuck = [];
+//fuck.push(t2);
 
-localStorage.setItem("tasko", JSON.stringify(fuck));
+//  localStorage.setItem("task", JSON.stringify(fuck));
 
-const data =  JSON.parse(localStorage.getItem("tasko"));
-const retrivedData = data.map(obj => {
-  return Object.assign({title: "", discribtion: "", dueDate: "", priority: "", list: "", state: false}, obj);
-});
-console.log("hamlik tasks " + retrivedData[0]);
-console.log("aewedi hak " + Array.isArray(retrivedData));
+console.log("giving the order to start creating");
+graphicHandler.reloadTasks();
+console.log("done creating");
+
+const retrivedData = localStorage.getItem("tasko")
+    ? JSON.parse(localStorage.getItem("tasko"))
+    : "empty";
+console.log("hamlik tasks ", JSON.parse(localStorage.getItem("task")));
+console.log("aewedi hak ", retrivedData);
 console.log(tasksHandler.getTasks());
 
 const tmpTasks = Object.values(localStorage.getItem("task"));
--- src/tasksHandler.js
+++ src/tasksHandler.js
@@ -10,41 +10,45 @@ function Task(title, discribtion, dueDate, priority, list, state) {
         state,
     };
 }
-//const task1 = Task("just do it", "nike", "today", "high", "Work", true);
-//const task2 = Task("just don't it", "adidas", "tomorow", "high", "Work", false);
+const task = Task("just don't it", "motivation", "now", "high", "Work", false);
 
 export const tasksHandler = (function () {
-    //let tasks = [task1, task2];
-
     let tasks = [];
+    const insilizer = () => {
+        tasks.push(task);
+        localStorage.setItem("task", JSON.stringify(tasks));
+    };
     
 
     const addTask = (title, discribtion, dueDate, priority, list, state) => {
-        const tmpTasks = Object.values(localStorage.getItem("task"))
-        console.log("chouf hna "+ Array.isArray(tmpTasks));
-        console.log("seyi seyi  "+ tmpTasks);
+        const tmpTasks = localStorage.getItem("task") ? JSON.parse(localStorage.getItem("task")) : [];
+        console.log("chouf hna ", tasks );
+        console.log("seyi seyi  ", tasks);
         tmpTasks.push(Task(title, discribtion, dueDate, priority, list, state));
-        localStorage.setItem("task", tmpTasks);
+        localStorage.setItem("task", JSON.stringify(tmpTasks));
         refresh();
     };
 
     const getTasks = () => {
+        console.log("i am getting the data ", JSON.parse(localStorage.getItem("task")));
+        refresh();
         return tasks;
     };
 
     const deleteTask = (index) => {
         tasks.splice(index, 1);
-        localStorage.setItem("task", tasks);
+        localStorage.setItem("task", JSON.stringify(tasks));
         refresh();
     };
 
     const refresh = () => {
-        tasks = Object.values(localStorage.getItem("task"));
+        tasks = localStorage.getItem("task") ? JSON.parse(localStorage.getItem("task")) : [];
+        
     };
 
     const changeTaskState = (index, state) => {
         tasks[index].state = state;
-        localStorage.setItem("task", tasks);
+        localStorage.setItem("task", JSON.stringify(tasks));
     };
 
     return {
@@ -52,5 +56,6 @@ export const tasksHandler = (function () {
         getTasks,
         deleteTask,
         changeTaskState,
+        insilizer
     };
 })();
